6366f692a3c7fd3dfad14164971a51b1722676c5
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-12-07 09:43:10 +0700

    feat: comment file

--- lib/watcher.go
+++ lib/watcher.go
@@ -14,18 +14,21 @@ func WactherFile(watcher *fsnotify.Watcher, extension string) {
 			if !ok {
 				return
 			}
-			if event.Op&fsnotify.Create == fsnotify.Create {
-				if strings.HasSuffix(event.Name, extension) {
-					log.Printf("New %s file detected: %s\n", extension, event.Name)
-					// Add your logic to upload data in real-time here
-				}
-			}
+
+			// Detect update file
 			if event.Op&fsnotify.Write == fsnotify.Write {
 				if strings.HasSuffix(event.Name, extension) {
-					log.Printf("New %s file update: %s\n", extension, event.Name)
+					log.Printf("New %s file update: %s\n", strings.ToUpper(extension), event.Name)
 					// Add your logic to upload data in real-time here
 				}
 			}
+
+			// Detect create file
+			// if event.Op&fsnotify.Create == fsnotify.Create {
+			// 	if strings.HasSuffix(event.Name, extension) {
+			// 		log.Printf("New %s file detected: %s\n", strings.ToUpper(extension), event.Name)
+			// 	}
+			// }
 		case err, ok := <-watcher.Errors:
 			if !ok {
 				return
