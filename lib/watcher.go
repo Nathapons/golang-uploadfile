2a9b25b15bb53a16566b123b4e13c5a98a5a1e60
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-12-07 09:34:06 +0700

    feat: check file is update

--- lib/watcher.go
+++ lib/watcher.go
@@ -16,7 +16,13 @@ func WactherFile(watcher *fsnotify.Watcher, extension string) {
 			}
 			if event.Op&fsnotify.Create == fsnotify.Create {
 				if strings.HasSuffix(event.Name, extension) {
-					log.Printf("New CSV file detected: %s\n", event.Name)
+					log.Printf("New %s file detected: %s\n", extension, event.Name)
+					// Add your logic to upload data in real-time here
+				}
+			}
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				if strings.HasSuffix(event.Name, extension) {
+					log.Printf("New %s file update: %s\n", extension, event.Name)
 					// Add your logic to upload data in real-time here
 				}
 			}
